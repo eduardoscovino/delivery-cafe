82c79bfd17ae52c85621c809e0b72005372ac3c0
eduardoscovino <eduardo.scovino@gmail.com>
2021-03-03 23:32:22 -0300

    repositorios de cafe e empregado criados

---
+++ cafe_repo.rb
@@ -0,0 +1,52 @@
+require "csv"
+require_relative "cafe"
+
+class CafeRepo
+  def initialize(csv_file)
+    @csv_file = csv_file
+    @cafes = []
+    @next_id = 1
+    load_csv if File.exist?(@csv_file)
+  end
+
+  def create(cafe)
+    cafe.id = @next_id
+    @cafes << cafe
+    @next_id += 1
+    save_csv
+  end
+
+  def all
+    @cafes
+  end
+  
+  def find_by_nome(nome)
+    @cafes.find { |cafe| cafe.nome == nome }
+  end
+
+  def find(id)
+    @cafes.find { |cafe| cafe.id == id }
+  end
+
+  private
+
+  def save_csv
+    CSV.open(@csv_file, "wb") do |csv|
+      csv << %w[id nome intensidade preco]
+      @meals.each do |meal|
+        csv << [cafe.id, cafe.nome, cafe.intensidade, cafe.preco]
+      end
+    end
+  end
+
+  def load_csv
+    csv_options = { headers: :first_row, header_converters: :symbol }
+    CSV.foreach(@csv_file, csv_options) do |row|
+      row[:id] = row[:id].to_i
+      row[:preco] = row[:preco].to_f
+      @cafes << Cafe.new(row)
+    end
+    @next_id = @cafes.last.id + 1 unless @cafes.empty?
+  end
+
+end
---
+++ empregado_repo.rb
@@ -0,0 +1,53 @@
+require 'csv'
+require_relative 'empregado'
+
+class EmpregadoRepo
+  def initialize(csv_file)
+    @csv_file = csv_file
+    @empregados = []
+    @next_id = 1
+    load_csv if File.exist?(@csv_file)
+  end
+
+  def create(empregado)
+    empregado.id = @next_id
+    @empregados << empregado
+    @next_id += 1
+    save_csv
+  end
+
+  def all
+    @empregados
+  end
+
+  def all_entregadores
+    @empregados.select { |empregado| empregado.funcao == "entregador" }
+  end
+
+  def find_by_username(usuario)
+    @empregados.find { |empregado| empregado.usuario == usuario}
+  end
+
+  def find(id)
+    @empregados.find { |empregado| empregado.id == id }
+  end
+
+  private
+
+  def save_csv
+    CSV.open(@csv_file, "wb") do |csv|
+      csv << %w[id nome usuario senha funcao]
+      @empregados.each do |empregado|
+        csv << [empregado.id, empregado.nome, empregado.usuario, empregado.senha, empregado.funcao]
+      end
+    end
+  end
+
+  def load_csv
+    csv_options = { headers: :first_row, header_converters: :symbol }
+    CSV.foreach(@csv_file, csv_options) do |row|
+      row[:id] = row[:id].to_i
+      @empregados << Empregado.new(row)
+    end
+    @next_id = @empregados.last.id + 1 unless @empregados.empty?
+end
